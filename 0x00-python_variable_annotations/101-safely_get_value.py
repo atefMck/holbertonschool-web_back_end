de2ac0ba333777169625f8fdd113df268ef4903f
atefsama666 <mechken.atef@gmail.com>
2021-03-16 15:53:19 +0000

    Task 11 Pep8 styled

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,10 @@ from typing import Mapping, Any, TypeVar, Union
 
 
 # The types of the elements of the input are not know
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[TypeVar('T'),
+                                    None] = None) -> Union[Any, TypeVar('T')]:
     """ azrfezaazdzad """
     if key in dct:
         return dct[key]
