3422d37227e50c530deaa7682597d563912c8f35
atefsama666 <mechken.atef@gmail.com>
2021-03-16 16:33:02 +0000

    Fixed Typo

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,5 +11,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """asqdsqdsqdsqdsqd"""
     coroutines = (wait_random(max_delay) for i in range(n))
-    lst = await asyncio.gather(*couroutines)
+    lst = await asyncio.gather(*coroutines)
     return sorted(lst)
