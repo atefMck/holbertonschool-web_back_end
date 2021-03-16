390e356c96925c47b7a50b95440bd8fc9d89151b
atefsama666 <mechken.atef@gmail.com>
2021-03-16 16:07:09 +0000

    Task 0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""asqdsqdsqdsqdsqd"""
+
+
+import asyncio
+import random 
+
+
+async def wait_random(max_delay: int = 10):
+    """asqdsqdsqdsqdsqd"""
+    sec = random.uniform(0, max_delay)
+    await asyncio.sleep(sec)
+    return sec
