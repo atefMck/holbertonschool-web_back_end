98d8c6ccf27747384158fc47fa462163cd53daa2
atefsama666 <mechken.atef@gmail.com>
2021-03-18 18:42:34 +0000

    Added annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,12 @@
 
 
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """zdqsdlksqjksq"""
     result = []
     async for i in async_generator():
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """zdqsdlksqjksq"""
     counter_before = time.perf_counter()
     coroutines = (async_comprehension() for i in range(4))
