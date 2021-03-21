d244b53e34470e07d617f5a02bd3823ac7be79c1
atefsama666 <mechken.atef@gmail.com>
2021-03-21 17:56:06 +0000

    Task 4

---
+++ 0x03-caching/4-mru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" Brosqhdkjsqhdnksjq """
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ 0-Brosqhdkjsqhdnksjq """
+    def __init__(self):
+        """ 0-Brosqhdkjsqhdnksjq """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ 0-Brosqhdkjsqhdnksjq """
+        if key is None or item is None:
+            return
+        if len(self.keys) >= self.MAX_ITEMS:
+            if key not in self.keys:
+                print("DISCARD: {}".format(self.keys[0]))
+                del self.cache_data[self.keys[0]]
+            del self.keys[0]
+        self.keys = [key] + self.keys
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ 0-Brosqhdkjsqhdnksjq """
+        if not key or key not in self.cache_data.keys():
+            return None
+        del self.keys[self.keys.index(key)]
+        self.keys = [key] + self.keys
+        return self.cache_data[key]
