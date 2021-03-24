40ac980afd780e64adb838923ce0e27565c1ebd3
atefsama666 <mechken.atef@gmail.com>
2021-03-24 09:32:01 +0000

    Task 0

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""qsdsqdsgkejokglkdsjsql"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """qsdsqdsgkejokglkdsjsql"""
+    return ((page - 1) * page_size, page * page_size)
