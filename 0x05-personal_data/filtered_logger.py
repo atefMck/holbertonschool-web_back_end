c872d6cb60b9dbd5b3525f4862ccddeee06fdb47
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-09 18:23:02 +0100

    fixed annotation

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -15,7 +15,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List):
+    def __init__(self, fields: List[str]):
         """ Braaaaaahsqqdsqdsqdsq """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
