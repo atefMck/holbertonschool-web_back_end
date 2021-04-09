595dc93ea98b5e3ee8fd23fd98608f0b5b95043f
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-09 21:15:29 +0100

    Task 6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Braaaaaahsqqdsqdsqdsq """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Braaaaaahsqqdsqdsqdsq """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
