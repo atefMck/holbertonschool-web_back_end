1a1fceb2fe21dc0ede8e42fd1b65013afc48390a
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-10 15:27:41 +0100

    Task 10

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 
 
+from models.user import User
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 import base64
 
@@ -25,8 +27,8 @@ class BasicAuth(Auth):
         return authorization_header.split(" ")[1]
 
     def decode_base64_authorization_header(
-         self,
-         base64_authorization_header: str) -> str:
+            self,
+            base64_authorization_header: str) -> str:
         """
             returns the decoded value of a Base64 string
         """
@@ -40,8 +42,8 @@ class BasicAuth(Auth):
             return None
 
     def extract_user_credentials(
-         self,
-         decoded_base64_authorization_header: str) -> (str, str):
+            self,
+            decoded_base64_authorization_header: str) -> (str, str):
         """
             returns the user credentials
         """
@@ -50,3 +52,19 @@ class BasicAuth(Auth):
                 or ":" not in decoded_base64_authorization_header):
             return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """
+            returns the User instance based on his email and password.
+        """
+        if (user_email is None or type(user_email) is not str
+                or user_pwd is None or type(user_pwd) is not str):
+            return None
+        user = User.search(attributes={"email": user_email})
+        if user:
+            if not user[0].is_valid_password(user_pwd):
+                return None
+            return user[0]
