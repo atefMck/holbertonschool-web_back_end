7aa62f423d40aeaf61d141f6a5947df2557aad94
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-10 15:13:48 +0100

    task 9

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -38,3 +38,15 @@ class BasicAuth(Auth):
                     .decode('utf-8'))
         except Exception:
             return None
+
+    def extract_user_credentials(
+         self,
+         decoded_base64_authorization_header: str) -> (str, str):
+        """
+            returns the user credentials
+        """
+        if (decoded_base64_authorization_header is None
+                or type(decoded_base64_authorization_header) is not str
+                or ":" not in decoded_base64_authorization_header):
+            return None
+        return tuple(decoded_base64_authorization_header.split(":"))
