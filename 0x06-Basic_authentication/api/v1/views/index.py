fa02e9bb4a6244f65a1bd22c7b03ef19248e81e0
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-10 13:16:27 +0100

    Task 2

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -27,9 +27,18 @@ def stats() -> str:
 
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauth() -> str:
+def unauthorized() -> str:
     """ GET /api/v1/unauthorized
     Return:
       - Raise 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - Raise 401 error
+    """
+    abort(403)
