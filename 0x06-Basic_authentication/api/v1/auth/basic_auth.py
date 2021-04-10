edb167100745c87d60c2255c3857879c4a865378
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-10 15:16:52 +0100

    Task 9 fixed

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,5 +48,5 @@ class BasicAuth(Auth):
         if (decoded_base64_authorization_header is None
                 or type(decoded_base64_authorization_header) is not str
                 or ":" not in decoded_base64_authorization_header):
-            return None
+            return (None, None)
         return tuple(decoded_base64_authorization_header.split(":"))
