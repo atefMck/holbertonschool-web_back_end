4127a86b89939e931b1581a24b9beff3f021d3ac
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-12 19:38:56 +0100

    pep8 styled

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -45,7 +45,7 @@ class Auth:
         return None
 
     def session_cookie(self, request=None):
-        """  
+        """
             Returns a cookie value from a request
         """
         if request is None:
