f4d264cc2197b3351ff6abb54b9a4afe7a663dc2
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-12 19:45:19 +0100

    Task 5

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -34,9 +34,11 @@ def check_auth():
     if auth is None or not auth.require_auth(request.path,
                                              ['/api/v1/status/',
                                               '/api/v1/unauthorized/',
-                                              '/api/v1/forbidden/']):
+                                              '/api/v1/forbidden/',
+                                              '/api/v1/auth_session/login/']):
         return
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) is None
+            and auth.session_cookie(request) is None):
         abort(401)
     elif auth.current_user(request) is None:
         abort(403)
