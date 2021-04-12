66b2456eb5c6f4998d0cecdf63ac96a105cff371
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-12 19:06:15 +0100

    Task 1

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module of session based authentication
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+        Session authentication Class
+    """
+
+    
