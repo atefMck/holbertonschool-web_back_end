59a787c1066dc2e5b11e2cd87ea8e7bf67599521
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-12 19:17:05 +0100

    Task 3

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -15,10 +15,18 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """
-        Session authentication Class
+        Creates Session for user_id
         """
         if user_id is None or type(user_id) is not str:
             return None
         session_id = uuid.uuid4()
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID based on a Session ID:
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
