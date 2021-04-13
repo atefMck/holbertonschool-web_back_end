1ff6d43c8c9b241c55e4813588251c264d7c84be
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-13 11:14:45 +0100

    Task 7

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Index views
+""" Module of Session Auth views
 """
 
 
@@ -9,22 +9,24 @@ from flask.globals import request
 from api.v1.views import app_views
 from models.user import User
 
-@app_views.route('/api/v1/auth_session/login', methods=['POST'], strict_slashes=False)
-def login():
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login():
+    """ Return the dictionary representation of the User
+    """
     email = request.form.get('email')
     if email is None or email == "":
-        return jsonify({{"error": "email missing"}}), 400
+        return jsonify({"error": "email missing"}), 400
     password = request.form.get('password')
     if password is None or password == "":
-        return jsonify({{"error": "password missing"}}), 400
+        return jsonify({"error": "password missing"}), 400
     user = User.search(attributes={"email": email})
-    if user is None:
-        return jsonify({ "error": "no user found for this email" }), 404
-    if not user.is_valid_password(password):
-        return jsonify({ "error": "wrong password" }), 401
+    if user is None or len(user) == 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
-    session_id = auth.create_session(user.id)
-    res = jsonify(user.to_json())
+    session_id = auth.create_session(user[0].id)
+    res = jsonify(user[0].to_json())
     res.set_cookie(os.getenv('SESSION_NAME'), session_id)
     return res
-    
