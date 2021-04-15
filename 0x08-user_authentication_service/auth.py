cb115983d5b139bea1e106affd7a1120ea9ab419
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-15 21:11:13 +0100

    Update auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -81,7 +81,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user if user else None
+            if user:
+                return user
+            return None
         except Exception:
             return None
 
