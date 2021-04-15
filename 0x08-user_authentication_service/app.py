a3e2642d607c38abfe92c4390db9047d11b9859a
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-15 20:59:27 +0100

    task 11

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,8 +4,10 @@ App module
 """
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from flask.globals import request
+from flask.helpers import make_response
+from flask.wrappers import Response
 from auth import Auth
 
 AUTH = Auth()
@@ -31,6 +33,20 @@ def users():
     except ValueError:
         return jsonify({
             "message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Register """
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = make_response(jsonify({{"email": email,
+                                           "message": "logged in"}}))
+        response.set_cookie('session_id', session_id)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
