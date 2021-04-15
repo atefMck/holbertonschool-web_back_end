ab6588115149fa43335db02d1913488fc7ab4ee6
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-15 17:31:22 +0100

    pep8 styled

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -7,10 +7,13 @@ App module
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def greetings():
     """ Greetings """
     return jsonify({'message': 'Bienvenue'})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
