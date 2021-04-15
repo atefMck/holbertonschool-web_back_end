ab72f55120fb1fc1e1dcb05be17259795f9db3aa
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-15 21:03:49 +0100

    Update app.py

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -37,7 +37,7 @@ def users():
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login():
-    """ Register """
+    """ Login method """
     email = request.form['email']
     password = request.form['password']
     if AUTH.valid_login(email, password):
@@ -50,4 +50,4 @@ def login():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="5000")
+    app.run(host="0.0.0.0", port="5000")
