e6476254655e5c96736256c594d0a5f2b7a06d47
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-15 21:23:37 +0100

    task 14

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,7 +4,7 @@ App module
 """
 
 
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, redirect
 from flask.globals import request
 from flask.helpers import make_response
 from flask.wrappers import Response
@@ -47,6 +47,20 @@ def login():
         response.set_cookie('session_id', session_id)
         return response
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ Logout method """
+    session_id = request.cookies.get('session_id')
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+        raise Exception
+    except Exception:
+        abort(403)
 
 
 if __name__ == "__main__":
