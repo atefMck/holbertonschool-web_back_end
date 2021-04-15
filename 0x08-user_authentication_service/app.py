3d38831a399d3da7498b3a37e55549f65a545c0a
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-15 21:40:58 +0100

    app fixed

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -23,13 +23,14 @@ def greetings():
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
     """ Register """
-    email = request.form['email']
-    password = request.form['password']
     try:
+        email = request.form['email']
+        password = request.form['password']
         user = AUTH.register_user(email, password)
-        return jsonify({
-            "email": email,
-            "message": "user created"})
+        if user:
+            return jsonify({
+                "email": email,
+                "message": "user created"})
     except ValueError:
         return jsonify({
             "message": "email already registered"}), 400
@@ -42,8 +43,8 @@ def login():
     password = request.form['password']
     if AUTH.valid_login(email, password):
         session_id = AUTH.create_session(email)
-        response = make_response(jsonify({{"email": email,
-                                           "message": "logged in"}}))
+        response = make_response(jsonify({"email": email,
+                                          "message": "logged in"}))
         response.set_cookie('session_id', session_id)
         return response
     else:
@@ -52,16 +53,14 @@ def login():
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
-    """
-    User logout function.
-    """
+    """ Logout method """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        abort(403)
-    else:
+    if user:
         AUTH.destroy_session(user.id)
-        return redirect('/')
+        return redirect("/")
+    else:
+        abort(403)
 
 
 if __name__ == "__main__":
