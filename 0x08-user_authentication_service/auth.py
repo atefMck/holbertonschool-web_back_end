6f8222fb5ee501cb51c1e932864e261092256ca1
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-16 14:03:50 +0100

    Update auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -111,7 +111,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             self._db.update_user(user.id,
-                                 password=_hash_password(password),
+                                 hashed_password=_hash_password(password),
                                  reset_token=None)
         except NoResultFound:
             raise ValueError
