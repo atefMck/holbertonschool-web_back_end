e540635ac34004864084bc1fb76f0002c844f8fb
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-26 15:54:34 +0100

    Task 1

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -15,5 +15,15 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence, res):
-        """ access_nested_map unit testing method """
+        """ access_nested_map result testing method """
         self.assertEqual(access_nested_map(nested_map, path), res)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self,
+                                         nested_map: Mapping,
+                                         path: Sequence):
+        """ access_nested_map exceptions testing method """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
