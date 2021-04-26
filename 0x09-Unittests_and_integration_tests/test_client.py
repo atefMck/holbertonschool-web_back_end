1dc0dfbbaef9d4438706307334ee169c327ef78b
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-26 17:11:41 +0100

    Task 6

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -28,8 +28,23 @@ class TestGithubOrgClient(unittest.TestCase):
 
         with patch('client.GithubOrgClient.org',
                    new_callable=PropertyMock,
-                   return_value= {'repos_url': 'http://bigBrain.url'}
+                   return_value={'repos_url': 'http://bigBrain.url'}
                    ) as patched:
             test_class = GithubOrgClient("BigBrain")
             result = test_class._public_repos_url
             self.assertEqual(result, patched.return_value['repos_url'])
+
+    @patch('client.get_json',
+           return_value={'repos_url': 'http://bigBrain.url'})
+    def test_public_repos(self, first_patch):
+        """ GithubOrgClient.public_repos unittesting """
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock,
+                   return_value=[]
+                   ) as second_patch:
+            test_class = GithubOrgClient("BigBrain")
+            result = test_class.public_repos(license="SmallBrain")
+            self.assertEqual(result, second_patch.return_value)
+            first_patch.assert_called_once()
+            second_patch.assert_called_once()
