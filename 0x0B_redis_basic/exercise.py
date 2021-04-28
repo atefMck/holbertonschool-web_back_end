2c75a6cc8432d41eae567aad43a7d12e7a84e9e6
Atef Mechken <54045904+atefsama666@users.noreply.github.com>
2021-04-28 12:59:48 +0100

    Task 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,9 +2,10 @@
 """
 Exercise file
 """
-from typing import Union
+from typing import Callable, Optional, Union
 import redis
 import uuid
+import sys
 
 
 class Cache:
@@ -20,3 +21,16 @@ class Cache:
         random_id = str(uuid.uuid4())
         self._redis.set(random_id, data)
         return random_id
+
+    def get(self, key: str,
+            fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """ Get originaltype data """
+        return fn(self._redis.get(key)) if fn else self._redis.get(key)
+
+    def get_str(self, value):
+        """ Converts bytes to string """
+        return value.decode("utf-8")
+
+    def get_int(self, value):
+        """ Converts bytes to integer """
+        return int.from_bytes(value, sys.byteorder)
