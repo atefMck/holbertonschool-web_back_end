531f7d47c1c2e3f5ee51e5fadd2595b79f4001bc
atefsama666 <mechken.atef@gmail.com>
2021-06-07 10:57:24 +0000

    more fixes

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+  if (typeof set !== 'object') return '';
+  if (typeof startString !== 'string') return '';
+  if (startString.length === 0) return '';
+
   const tmp = [];
   set.forEach((element) => {
     if (startString !== '' && element.startsWith(startString)) {
