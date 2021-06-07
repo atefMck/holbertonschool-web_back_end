9d7f8f70779c815289b1a0057e8372b549a19243
atefsama666 <mechken.atef@gmail.com>
2021-06-07 20:34:50 +0000

    task 5 fixed

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -5,11 +5,11 @@ const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   const { url } = req;
-  if (url === '/') res.end('Hello Holberton School!\n');
+  if (url === '/') res.end('Hello Holberton School!');
   else if (url === '/students') {
     countStudents(process.argv[2])
       .then((message) => {
-        const response = `This is the list of our students\n${message}\n`;
+        const response = `This is the list of our students\n${message}`;
         res.end(response);
       })
       .catch((err) => {
