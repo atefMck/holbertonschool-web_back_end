e6eb73178e2ae515fbc66de4f035bfce71f4487b
atefsama666 <mechken.atef@gmail.com>
2021-06-07 13:59:15 +0000

    task 2 fixed

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
+  try {
+    const data = fs.readFileSync(path, 'utf8');
     const fields = {};
     const students = data.split('\n').map((student) => student.split(','));
     students.shift();
 
     console.log(`Number of students: ${students.length}`);
+
     students.forEach((student) => {
       if (!fields[student[3]]) fields[student[3]] = [];
       fields[student[3]].push(student[0]);
@@ -17,5 +16,7 @@ module.exports = function countStudents(path) {
     Object.keys(fields).forEach((key) => {
       console.log(`Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`);
     });
-  });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 };
