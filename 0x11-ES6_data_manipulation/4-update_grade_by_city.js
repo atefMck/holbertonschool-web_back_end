4e49e2a0b9e4de9a8d6b39ffaf88e67b63ba96da
atefsama666 <mechken.atef@gmail.com>
2021-06-07 10:49:21 +0000

    Task 4 fixed

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   if (Array.isArray(students)) {
     return students.filter((student) => student.location === city).map((student) => {
       const grade = newGrades.find((grade) => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: grade?.grade || 'N/A',
-      };
+      return grade !== undefined ? { ...student, grade: grade.grade } : { ...student, grade: 'N/A' };
     });
   }
   return 0;
