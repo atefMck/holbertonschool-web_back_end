acb776f8ef85fea8e9e38aab154468bf433ff4da
Atef Mechken <54045904+atefMck@users.noreply.github.com>
2021-11-15 15:39:11 +0100

    checking documentation issue

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
-"""DB module
+"""
+    Database control class
+> Is able to create/delete users
 """
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
+from user import User, Base
 from sqlalchemy.exc import InvalidRequestError
-
-from user import Base, User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
-    """DB class
-    """
+    """ Database engine class (DB)"""
 
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,38 +32,47 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Save the user to the database
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        [Summary]
+        > Returns a User object that is simultaneously saved
+        to the database
+        Args:
+            email (str): Email for the new user instance
+            hashed_password (str): The new hashed password for the instance
+        Returns:
+            User: Returns a User object
+        """
+        DBSession = self._session
+        new_user = User(email=email, hashed_password=hashed_password)
+        DBSession.add(new_user)
+        DBSession.commit()
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """
-        Returns the first row found in the users
-        table as filtered by the method’s input arguments
+            Finds a User in the DB based on kwargs:
+https://docs.sqlalchemy.org/en/13/orm/query.html#sqlalchemy.orm.query.Query.filter_by
+            Returns the first row found in the users table as filtered by
+            the method’s input arguments
         """
-        valid_args = ['id', 'email', 'hashed_password',
-                      'session_id', 'reset_token']
-        for k in kwargs.keys():
-            if k not in valid_args:
-                raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
+        DBSession = self._session
+        query = DBSession.query(User).filter_by(**kwargs)
+        result = query.first()
+        if (result is None):
             raise NoResultFound
-        return user
+        return result
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
-        Updates User and saves in database
+            Updates a user using **kwargs. Returns None.
+            Will raise a NoResultFound error if user doesnt exist
+            If an attribute is passed that isnt in attributes it will
+            raise a ValueError
         """
-        valid_args = ['id', 'email', 'hashed_password',
-                      'session_id', 'reset_token']
+        DBSession = self._session
         user = self.find_user_by(id=user_id)
-        if user:
-            for k in kwargs.keys():
-                if k not in valid_args:
-                    raise ValueError
-                setattr(user, k, kwargs[k])
-            self._session.commit()
+        for key, value in kwargs.items():
+            if hasattr(user, key) is False:
+                raise ValueError
+            setattr(user, key, value)
+        DBSession.commit()
