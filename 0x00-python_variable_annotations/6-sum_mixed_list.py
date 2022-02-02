e018ee1969d274137e0dd71e9a38a78e25727bdc
Atef Mechken <54045904+atefMck@users.noreply.github.com>
2022-02-02 15:07:13 +0100

    Update 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,6 @@
 from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ azrfezaazdzad """
     return sum(mxd_lst)
